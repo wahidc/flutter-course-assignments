4026fbd39938be843a27c17dd5bdbac705d47308
Wahid Chowdhury <wahidchowdhury2007@gmail.com>
2026-06-23 21:07:40 +0600

    Added Spread operator part and fixed naming convension

--- week02_student_management_mini_project/student_management_mini_project.dart
+++ week02_student_management_mini_project/student_management_mini_project.dart
@@ -2,23 +2,21 @@ void main() {
   bool isNewStudent = true;
 
   // Created a List of student names and print it.
-  List<String> students_name = [
+  List<String> studentNames = [
     "Tonmoy",
     "Sakib",
     "Rahat",
     "Nafis",
     if (isNewStudent) "Rahim", // Used 'Collection if'
   ];
-  print("Students: \n ${students_name} \n");
-
+  print("Students: \n ${studentNames} \n");
 
   // Created a Set of enrolled courses and print it.
-  Set<String> enrolled_courses = {"Flutter", "Dart", "Git"};
-  print("Courses: \n ${enrolled_courses} \n");
-  
+  Set<String> enrolledCourses = {"Flutter", "Dart", "Git"};
+  print("Courses: \n ${enrolledCourses} \n");
 
   // Created a Map where 'Key = Student Name' and 'Value = Student Age' and print it.
-  Map<String, int> student_age = {
+  Map<String, int> studentAge = {
     "Tonmoy": 22,
     "Sakib": 23,
     "Rahat": 21,
@@ -26,7 +24,12 @@ void main() {
     "Rahim": 20,
   };
   print("Student Ages:");
-  for (var entry in student_age.entries) {
+  for (var entry in studentAge.entries) {
     print("${entry.key} -> ${entry.value}");
   }
+
+  // Used the Spread operator to combine 2 lists.
+  List<String> list1 = ["Vermithor", "Vhagar", "Moondancer"];
+  List<String> list2 = ["Sheepstealer", "Vermax", ...list1];
+  
 }
